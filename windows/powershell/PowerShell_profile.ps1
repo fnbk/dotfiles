369e251f0ccf7610a4fc97be7a429cab62131bff
Florian Boehmak <Florian.Boehmak@plan-b-gmbh.com>
2019-07-11 11:36:46 +0200

    add powershell profile

---
+++ windows/powershell/PowerShell_profile.ps1
@@ -0,0 +1,8 @@
+# aliases
+Set-Alias -Name k -Value C:\Users\florianb\.azure-kubectl\kubectl.exe
+Set-Alias -Name f -Value C:\ProgramData\chocolatey\bin\fluxctl.exe
+Set-Alias -Name edit -Value "C:\Program Files\Notepad++\notepad++.exe"
+
+# done
+Write-Host "Custom PowerShell Environment Loaded, find me at:"
+Write-Host "C:\Users\florianb\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
---
+++ windows/powershell/README.md
@@ -0,0 +1,9 @@
+
+find out the current path to the powershell profile
+```
+$profile
+```
+
+my current path: C:\Users\florianb\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+
+
