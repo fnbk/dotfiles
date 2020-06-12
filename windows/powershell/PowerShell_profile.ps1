fe31dc5cfc9a78d827b3785e68d8f868664067f7
Florian Boehmak <florian@fnbk.cc>
2020-06-12 23:53:40 +0200

    add alias

--- windows/powershell/PowerShell_profile.ps1
+++ windows/powershell/PowerShell_profile.ps1
@@ -1,8 +1,10 @@
 # aliases
-Set-Alias -Name k -Value C:\Users\florianb\.azure-kubectl\kubectl.exe
+Set-Alias -Name k -Value C:\ProgramData\chocolatey\bin\kubectl.exe
 Set-Alias -Name f -Value C:\ProgramData\chocolatey\bin\fluxctl.exe
 Set-Alias -Name edit -Value "C:\Program Files\Notepad++\notepad++.exe"
+New-Alias which get-command
 
 # done
-Write-Host "Custom PowerShell Environment Loaded, find me at:"
-Write-Host "C:\Users\florianb\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
+Write-Host "Custom PowerShell Environment Loaded, please edit me at:"
+Write-Host "C:\Users\flori\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
+Write-Host ""
