8c2b4bb22181bc0ea81f7b679f5ab6531dade9d3
Florian Boehmak <florian@fnbk.cc>
2020-06-18 11:23:47 +0200

    edit-hosts in elevated mode

--- windows/powershell/PowerShell_profile.ps1
+++ windows/powershell/PowerShell_profile.ps1
@@ -1,10 +1,15 @@
 
 #
-# helper functions
+# functions
 #
 
-Function EditHostsFile {
-	C:\"Program Files"\Notepad++\notepad++.exe c:\Windows\System32\Drivers\etc\hosts
+Function Edit-Hosts {
+	# execute notepad++ in elevated mode
+    Start-Process C:\"Program Files"\Notepad++\notepad++.exe -Verb runAs -ArgumentList c:\Windows\System32\Drivers\etc\hosts
+}
+
+Function Edit-Profile {
+	C:\"Program Files"\Notepad++\notepad++.exe C:\Users\flori\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
 }
 
 
@@ -16,8 +21,8 @@ Set-Alias -Name k -Value C:\ProgramData\chocolatey\bin\kubectl.exe
 Set-Alias -Name f -Value C:\ProgramData\chocolatey\bin\fluxctl.exe
 Set-Alias -Name edit -Value "C:\Program Files\Notepad++\notepad++.exe"
 New-Alias which get-command
-Set-Alias -Name hosts -Value EditHostsFile
-
+Set-Alias -Name hosts -Value Edit-Hosts
+Set-Alias -Name hosts -Value Edit-Profile
 
 #
 # done
@@ -27,3 +32,10 @@ Set-Alias -Name hosts -Value EditHostsFile
 Write-Host "Custom PowerShell Environment Loaded, please edit me at:"
 Write-Host "C:\Users\flori\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
 Write-Host ""
+
+
+#
+# good examples
+#
+
+# https://gist.github.com/timsneath/19867b12eee7fd5af2ba
